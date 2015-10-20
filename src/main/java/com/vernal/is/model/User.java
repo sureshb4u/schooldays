4fb49b34bd751b05fe4c6f7e471f91d7ab56aeb2
vignesh <viki19nesh@gmail.com>
2015-10-20 23:05:07 +0530

    removed from user model

--- src/main/java/com/vernal/is/model/User.java
+++ src/main/java/com/vernal/is/model/User.java
@@ -1,21 +1,12 @@
 package com.vernal.is.model;
 
 public class User extends Person{
-	private String id;
 	private String personId;
 	private String sessionToken;
 	private String userName;
-	private String dateOfBirth;
-	private String gender;
 	private String userRole;
 	private String imageUrl;
 	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
 	public String getImageUrl() {
 		return imageUrl;
 	}
@@ -30,18 +21,6 @@ public class User extends Person{
 	}
 	public String getUserName() {
 		return userName;
-	}
-	public String getDateOfBirth() {
-		return dateOfBirth;
-	}
-	public void setDateOfBirth(String dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
